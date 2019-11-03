bc7e7cb8a5acfb44229f3c7f8193e58d9bbed939
whiteGloom <karabin.m@yandex.ru>
2019-11-03 19:06:34 +0700

    v0.3.16 patch
    Список изменений:
    • добавлен плагин webpack-loader;
    • обновлён конфиг webpack;

--- index.js
+++ index.js
@@ -1,21 +1,68 @@
-import fs from "fs";
+import removeServiceOutputsPlugin from "remove-service-outputs-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import WebpackLoader from "webpack-loader";
 import colors from "colors/safe";
+import fs from "fs";
 
 import npmHelper from "./src/npmHelper.js";
 import PathsLoader from "./src/PathsLoader.js";
-import WebpackLoader from "./src/webpackData/WebpackLoader.js";
 
 const pathsLoader = new PathsLoader();
 const webpackLoader = new WebpackLoader();
+
+//
+// Config
+//
 
 const npmArguments = process.argv.slice(2);
 const workFolder = process.cwd();
 const pathsFile = "profilePaths.txt";
 
 // Webpack config
-var cssOutputName = "userChrome",
-	entryChunkName = "stylesLoader";
-webpackLoader.makeConfig({workFolder, entryChunkName, cssOutputName});
+var entryChunkName = "stylesLoader",
+	cssOutputName = "userChrome.css";
+	
+webpackLoader.makeNewConfig("main", {
+	entry: {
+		stylesLoader: workFolder + "/src/stylesLoader.js"
+	},
+	output: {
+		path: workFolder + "/prod/",
+		filename: data => {
+			switch (data.chunk.name) {
+				default:
+					return "[name].js"
+			}
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(css)/,
+				use: [
+					"style-loader",
+					MiniCssExtractPlugin.loader,
+					{ loader: "css-loader", options: { url: false } }
+				],
+				exclude: /[\\/]node_modules[\\/]/
+			}
+		]
+	},
+	plugins: [
+		new removeServiceOutputsPlugin([
+			[entryChunkName, /.*\.js$/]
+		]),
+		new MiniCssExtractPlugin({
+			filename: cssOutputName
+		})
+	],
+	devtool: "none"
+}, "development");
+
+
+//
+// Init part
+//
 
 // Simple build of styles
 if (npmHelper.checkTag(npmArguments, "simpleBuild")) simpleBuildMode();
@@ -42,10 +89,10 @@ function stylesAutoApplyMode() {
 		paths.forEach(path => {
 			try {
 				fs.statSync(path + "/chrome/");
-				fs.copyFileSync("./prod/" + cssOutputName + ".css", path + "/chrome/" + cssOutputName + ".css");
+				fs.copyFileSync("./prod/" + cssOutputName, path + "/chrome/" + cssOutputName);
 			} catch(err) {
 				fs.mkdirSync(path + "/chrome/");
-				fs.copyFileSync("./prod/" + cssOutputName + ".css", path + "/chrome/" + cssOutputName + ".css");
+				fs.copyFileSync("./prod/" + cssOutputName, path + "/chrome/" + cssOutputName);
 			} finally {
 				console.log(colors.green("The styles was successfully copied to: ") + colors.underline.green(path + "/chrome/"));
 			};
